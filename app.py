b123bc8938b33dede51bd51b71ce8489766ee931
ssharma0484 <ssharma01x@gmail.com>
2026-07-04 17:37:12 +0530

    Initialize Flask app with home and health routes

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/health")
+def health():
+    return "Healthy"
+
+if name == "__main__":
+    app.run(host="0.0.0.0", port=5000)
